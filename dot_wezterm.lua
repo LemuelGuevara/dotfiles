1a8896c068a88bcc52ad2c5892bdf95dbcfd362b
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-06-12 18:41:37 +0800

    refactor: updated wezterm font

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font("SFMono Nerd Font")
 config.font_size = 14
 
 config.enable_tab_bar = false
