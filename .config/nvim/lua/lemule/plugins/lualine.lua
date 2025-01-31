fb4e75408bcc58d5884ca81d7571cdaa9dd97e86
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2025-01-31 20:43:12 +0800

    refactor(lualine): updated theme

--- .config/nvim/lua/lemule/plugins/lualine.lua
+++ .config/nvim/lua/lemule/plugins/lualine.lua
@@ -2,34 +2,6 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		local colors = {
-			blue = "#89b4fa",
-			cyan = "#74c7ec",
-			black = "#313244",
-			white = "#cdd6f4",
-			red = "#f38ba8",
-			violet = "#cba6f7",
-			grey = "#313244",
-		}
-
-		local catpuccin_mocha = {
-			normal = {
-				a = { fg = colors.black, bg = colors.violet },
-				b = { fg = colors.white, bg = colors.grey },
-				c = { fg = colors.white },
-			},
-
-			insert = { a = { fg = colors.black, bg = colors.blue } },
-			visual = { a = { fg = colors.black, bg = colors.cyan } },
-			replace = { a = { fg = colors.black, bg = colors.red } },
-
-			inactive = {
-				a = { fg = colors.white, bg = colors.black },
-				b = { fg = colors.white, bg = colors.black },
-				c = { fg = colors.white },
-			},
-		}
-
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
