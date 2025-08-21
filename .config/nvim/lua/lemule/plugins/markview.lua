c7a3dc35b114251dc2c88b2281864a3c522f5777
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2025-08-21 17:58:45 +0800

    fix(markview): fixed lazy startup bug

--- .config/nvim/lua/lemule/plugins/markview.lua
+++ .config/nvim/lua/lemule/plugins/markview.lua
@@ -1,10 +1,9 @@
 return {
 	"OXY2DEV/markview.nvim",
-	lazy = false, -- Recommended
-	-- ft = "markdown" -- If you decide to lazy-load anyway
+	event = "VeryLazy",
 
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
+		-- "nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
 	},
 }
